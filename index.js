eb8d43ca6cb406da0c830a3e5c3a70c1e7bffca8
chenjing <pyramid102@sina.com>
2019-01-28 09:37:48 +0800

    创建项目结构

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 /**小肥牛扫码点餐项目API子系统 */
+console.log('准备启动API服务器');
+console.log(new Date().toLocaleString())
+
 const PORT = 8090;
 const express =require('express');
 
+//创建HTTP应用服务器
 //启动主服务器
 var app=express();
 app.listen(PORT,()=>{
-  console.log('Server Listening '+PORT+' ...')
+  console.log('API服务器启动成功');
+  console.log('Server Listening '+PORT+' ...');
 })
+
